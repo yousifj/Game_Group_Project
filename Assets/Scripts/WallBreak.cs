94000e52bdced532db7453f78f2249fd0099fc1d
Rafael Loo <loorafael@hotmail.com>
2024-05-09 00:39:29 -0700

    new rhino sound, added comments to rhino and wallbreak scripts

--- Assets/Scripts/WallBreak.cs
+++ Assets/Scripts/WallBreak.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
     
+// Script to destroy object upon the destruction of set gameobject
 public class WallBreak : MonoBehaviour
 {
     public GameObject enemy; // Assign the enemy object in the Unity inspector
