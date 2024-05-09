a8761d8403b5710366ce0a6cb4dc11e1c31aeb79
Rafael Loo <loorafael@hotmail.com>
2024-05-08 22:59:23 -0700

    fixed rhino script added sounds and animations

--- Assets/Scripts/RhinoEnemy.cs
+++ Assets/Scripts/RhinoEnemy.cs
@@ -5,45 +5,68 @@ using UnityEngine;
 public class RhinoEnemy : MonoBehaviour
 {
     public Transform player;
+    // Current state
+    public State rhinoState;
+    // Speed when in patrolling state
     public float patrolSpeed = 2f;
-    public float boostedSpeed = 10f; // Speed during the boost
-    public float detectionRadius = 5f; // This could be the trigger radius for boosting
-    public float bounceForce = 300f; // Amount of force to apply for the bounce, adjustable in Inspector
+    // Speed during the boost
+    public float boostedSpeed = 10f;
+    // Trigger radius for dashing
+    public float detectionRadius = 5f; 
+    // Amount of force to apply for the bounce, adjustable in Inspector
+    public float bounceForce = 10f; 
+    // Delay after hitting wall
     public float wallHitDelay = 1.5f;
-    public float boostDelay = .5f;
-    public float bounceDelay = .5f;
-    private Vector2 patrolDirection = Vector2.right;
+    // Delay before starting dash
+    public float boostDelay = .5f; 
+    // Delay before returning to patrol mode
+    public float fatigueDuration = .5f;
+
+    private Vector2 patrolDirection = Vector2.left;
+
     private Rigidbody2D rb;
+
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    //This represents the audio manager game object 
+    AudioManager audioManager;
 
     public enum State
     {
         Patrolling,
-        Boosting,
-        BouncingBack
+        Dashing,
+        Fatigued,
     }
-
-    private State currentState = State.Patrolling;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rhinoState = State.Patrolling;
+        animator = GetComponent<Animator>();
+        //Find the audio manager game object
+        audioManager = FindAnyObjectByType<AudioManager>().GetComponent<AudioManager>();
+        
     }
 
     void Update()
     {
-        switch (currentState)
+        switch (rhinoState)
         {
             case State.Patrolling:
+                animator.SetBool("Dashing", false);
+                animator.SetBool("WallHit", false);
                 Patrol();
                 CheckForBoostTrigger();
                 break;
-            case State.Boosting:
+            case State.Dashing:
+                animator.SetBool("Dashing", true);
                 Boost();
                 break;
-            case State.BouncingBack:
-                rb.velocity = new Vector2(0 * patrolDirection.x, rb.velocity.y);
+            case State.Fatigued:
+                
+                rb.velocity = new Vector2(0 * patrolDirection.x, rb.velocity.y); //Stops movement
                 break;
         }
     }
@@ -69,52 +92,49 @@ public class RhinoEnemy : MonoBehaviour
 
     void Boost()
     {
-        rb.velocity = new Vector2(boostedSpeed * patrolDirection.x, rb.velocity.y);
-        
+        rb.velocity = new Vector2(boostedSpeed * patrolDirection.x, rb.velocity.y); // Increases speed
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Wall")
         {
+            audioManager.Play(AudioManager.Sound.RhinoDamage); // Plays wall hit sound
             Vector2 bounceDirection = -rb.velocity.normalized; // Reverse the current direction
             bounceDirection += Vector2.up; // Add an upward force component
             bounceDirection.Normalize(); // Normalize to ensure consistent force application
 
-            rb.AddForce(bounceDirection * bounceForce, ForceMode2D.Impulse);            
-            BounceBack();
+            rb.AddForce(bounceDirection * bounceForce, ForceMode2D.Impulse);
+            StartCoroutine(Recovering());
         }
     }
-
-    void BounceBack()
-    {
-        currentState = State.BouncingBack;
-        StartCoroutine(BounceDelay());
-    }
-
+    // Flips rhino sprite
     void FlipSprite(float direction)
     {
         spriteRenderer.flipX = direction < 0;
     }
 
-    IEnumerator ResetToPatrol()
-    {
-        
-        yield return new WaitForSeconds(wallHitDelay); // Add a brief delay
-        patrolDirection = -patrolDirection; // Flip patrol direction
-        currentState = State.Patrolling;
-    }
-
+    // Delays boost
     IEnumerator DelayedBoostStart()
     {
         yield return new WaitForSeconds(boostDelay); // Wait for the specified delay
-        currentState = State.Boosting;
+        rhinoState = State.Dashing;
     }
 
-    IEnumerator BounceDelay()
+    // Delays time before Rhino recovers
+    IEnumerator Recovering()
     {
-        yield return new WaitForSeconds(bounceDelay); // Wait for the specified delay
-        currentState = State.BouncingBack;
+        animator.SetBool("WallHit", true);
+        yield return new WaitForSeconds(fatigueDuration); // Wait for the specified delay
+        rhinoState = State.Fatigued;
         StartCoroutine(ResetToPatrol());
+    }
+
+    // Resets state to patrolling
+    IEnumerator ResetToPatrol()
+    {
+        yield return new WaitForSeconds(wallHitDelay); // Add a brief delay
+        patrolDirection = -patrolDirection; // Flip patrol direction
+        rhinoState = State.Patrolling;
     }
 }
